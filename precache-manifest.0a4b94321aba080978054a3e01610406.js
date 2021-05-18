b9e462e84fcc233d933e961cb846ea5fd74b81c8
Olga Tsarova <alita.jet@gmail.com>
2021-05-18 17:08:49 +0300

    Updates

---
+++ precache-manifest.0a4b94321aba080978054a3e01610406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9694ee2a3c7d00255a35639d19a7fe3b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd0560a048beb78f9db",
+    "url": "./static/js/main.2b425978.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
